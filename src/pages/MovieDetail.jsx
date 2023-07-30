20d3691a55bd2f09744edafd594e7eaa6d4b7eb0
Mcclayn798 <mnkmcc9@gmail.com>
2023-07-30 14:43:53 +0300

    video key

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,41 +1,52 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+
 const MovieDetail = () => {
+
   const API_KEY = "b1ad040c8b59ee92603839b9fbbe4adf";
   const { id } = useParams()
-  const [moviedetail, setMoviedetail] = useState('')
-  const [detail, setMoviedetail] = useState('')
+  const [moviedetail, setMoviedetail] = useState({
+    details:"",
+    key:""
+  })
+
   const DETAIL_URL = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`
-  const VİDEO_URL = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`
-
-
+  const VIDEO_URL = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`
 
   const getData = async () => {
     try {
       const response = await axios(DETAIL_URL)
-      console.log(response);
-      setMoviedetail(response?.data)
+      const video = await axios(VIDEO_URL)
+      setMoviedetail({...moviedetail, details:response?.data, key: video.data.results[0].key} )
     } catch (error) {
       console.log(error);
     }
   }
-  console.log(moviedetail);
+
   useEffect(() => {
     getData()
   }, [])
+
   const { title,overview, poster_path, vote_average, vote_count
-    , release_date } = moviedetail
+    , release_date } = moviedetail.details
+
+console.log(`https://www.youtube.com/watch?v=${moviedetail.key}`);
   return (
     <div className="bg-slate-600">
+
       <div className=" container mx-auto   w-12/12  py-5 ">
-        <h1 className="text-center mb-2 text-3xl">{title}</h1>
+
+        <h1 className="text-center mb-5 text-3xl ">{title}</h1>
+
         <div className="w-8/12 md:w-3/5 mx-auto">
           <div className="ratio h-[400px] ratio-16x9">
-            <iframe className="rounded-xl" src="" title="YouTube video" allowFullScreen __idm_id__="32769"></iframe>
+            <iframe className="rounded-xl" src={`https://www.youtube.com/embed/${moviedetail.key}`} title="YouTube video" allowFullScreen __idm_id__="32769"></iframe>
           </div>
         </div>
+
         <div className="container flex justify-center mt-20">
+
           <div className="flex flex-col lg:flex-row max-w-6xl rounded-lg bg-gray-100 shadow-lg">
             <img className="lg:w-1/3 h-96 lg:h-[600px] object-cover rounded-t-lg md:rounded-none md:rounded-l-lg " src={`https://image.tmdb.org/t/p/w1280${poster_path}`} alt="" />
             <div className="p-6 flex flex-col justify-between">
@@ -55,16 +66,20 @@ const MovieDetail = () => {
                   </tr>
                   <tr className="border-b-2 border-gray-500 ">
                     <td className="p-2 text-blue-800 underline underline-offset-4">
-                      <Link to={-1}>Go Back</Link>
+                      <Link to={-1}>Go Back</Link> 
                     </td>
                   </tr>
                 </tbody>
               </table>
             </div>
           </div>
+
         </div>
+
       </div>
     </div>
+
   );
 };
+
 export default MovieDetail;
