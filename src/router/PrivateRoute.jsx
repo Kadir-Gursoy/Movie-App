ac08db4295a84e74eb5d726cccc9c9d8b5ef0a7b
Mcclayn798 <mnkmcc9@gmail.com>
2023-08-01 22:51:52 +0300

    privateroute

--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -1,7 +1,9 @@
 
+import { useContext } from 'react';
 import { Navigate, Outlet } from 'react-router-dom'
 
 const PrivateRoute = () => {
+  const {user} = useContext(AudioContext);
   const user = true;
   return (
     user ? <Outlet /> : <Navigate to="/login"/>
